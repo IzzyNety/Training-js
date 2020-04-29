77f95186539a8a6d048aa76d6872cfd9212c539f
IzzyNety <izzy.nety@gmail.com>
2020-04-29 09:58:31 +0500

    улучшение и завершение проекта

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -5,6 +5,7 @@ const modals = () => {
           modal = document.querySelector(modalSelector),
           close = document.querySelector(closeSelector),
           windows = document.querySelectorAll('[data-modal]');
+          scroll = calcScroll();
 
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
   
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
         /* document.body.classList.add('modal-open'); */
       });
     });
@@ -30,6 +32,7 @@ const modals = () => {
 
       modal.style.display = "none";
       document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
     /*  document.body.classList.remove('modal-open'); */
     });
     
@@ -42,6 +45,7 @@ const modals = () => {
   
         modal.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
       /* document.body.classList.remove('modal-open'); */
       }
     });
@@ -53,6 +57,22 @@ const modals = () => {
       document.querySelector(selector).style.display = 'block';
       document.body.style.overflow = "hidden";
     }, time);
+  }
+
+  /* подсчитывать растояние в пикселях */
+  function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetHeight - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
   }
  
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
